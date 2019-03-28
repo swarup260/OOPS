9aecf5c085aec5bcbb57024e7d56f3891e32d148
swarup <swarup260@gmail.com>
2019-03-28 22:52:09 +0530

    V1 version using builder Pattern

---
+++ src/V1/product.ts
@@ -0,0 +1,54 @@
+import { ProductBuilder } from "./ProductBuilder";
+import { ProductBuilderInterFace } from "./ProductBuilderInterFace";
+
+export class Product {
+  public name: string;
+  public sku: string;
+  public id: number;
+  public qty: number;
+  public price: number;
+  public specialPrice: number;
+  public status: boolean;
+  public creationDate: Date;
+  public updationDate: Date;
+  public type: string;
+  private _attributSets: any;
+
+  constructor(product: ProductBuilderInterFace) {
+    this.name = product.name;
+    this.sku = product.sku;
+    this.id = product.id;
+    this.price = product.price;
+    this.qty = product.qty || 0;
+    this.specialPrice = product.specialPrice || 0.0;
+    this.status = product.status || true;
+    this.creationDate = product.creationDate || new Date();
+    this.updationDate = product.updationDate || new Date();
+    this.type = product.type || "simple";
+    this._attributSets = product.attributSets || new Array();
+  }
+
+  public addAttribute(attribute): Product {
+    this._attributSets.push({
+      attributeType: attribute.key,
+      attribueValue: attribute.value
+    });
+    return this;
+  }
+
+  public get _attributeSets(): any {
+    return this._attributSets;
+  }
+
+  public getAttributeValue(key: string): any {
+    for (const attribute of this._attributSets) {
+      if (attribute.attributeType == key) {
+        return attribute.attribueValue;
+      }
+    }
+  }
+
+  public toString(): string {
+    return JSON.stringify(this, undefined, 3);
+  }
+}
